d995cbe264a46abe0ef082e9d485e28280cc00f5
Ben DeWeerd <bendeweerd2001@gmail.com>
2022-01-12 16:47:43 -0500

    add basic file copying capability

--- Project_1/copy.c
+++ Project_1/copy.c
@@ -1,14 +1,45 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    if(argc == 3){
-        printf("Source: %s\n", argv[1]);
-        printf("Destination: %s\n", argv[2]);
+    //check for the correct number of arguments (3)
+    if(argc != 3){
+        perror("Error: incorrect number of arguments");
+        return -1;
     }
-    else {
+    //check that the source file is a normal file
+    
+    //check the destination file doesn't already exist
+    if(access(argv[2], F_OK) == 0){        
+        perror("Error: destination file must not already exist");
+        return -1;
+    }
+
+    //everything is ok so far, try to open the source file
+    FILE *source;
+    source = fopen(argv[1], "r");
+    if(source == NULL){
         perror("Error: ");
         return -1;
     }
+    
+    //create destination file
+    FILE *destination;
+    destination = fopen(argv[2], "w");
+
+    //copy the file one character at a time
+    char c;
+    c = fgetc(source);
+    while(c != EOF){
+        fprintf(destination, "%c", c);
+        c = fgetc(source);
+    }
+    printf("\n\n");
+
+    //done with the file; close it
+    fclose(source);
+    fclose(destination);
+
     return 0;
 }
 
