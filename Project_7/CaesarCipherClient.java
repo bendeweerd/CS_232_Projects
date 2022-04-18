b421b2289761d217b489020b66676dae6b79d922
Ben DeWeerd <bendeweerd2001@gmail.com>
2022-04-18 15:14:25 -0400

    add remaining functionality, comments

--- Project_7/CaesarCipherClient.java
+++ Project_7/CaesarCipherClient.java
@@ -1,10 +1,27 @@
+/*
+CaesarCipherClient.java
+CS 232 Client-Server Computing Project
+
+Created by Ben DeWeerd on 4.18.2022
+
+The CaesarCipherClient class creates a client to interact with
+CaesarCipherServer objects. After establishing a connection and
+determining the shift amount, the client accepts input, sends it
+to the server for encoding, receives the server's response, and
+displays the result.
+
+The server uses the standard Socket class for networking and 
+and the Scanner class to accept user input.
+
+The tutorials found here were very helpful:
+https://www.infoworld.com/article/2853780/socket-programming-for-scalable-systems.html
+*/
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
-
-//TODO: exit with message when connection to server is lost
 
 class CaesarCipherClient{
     public static void main(String args[])
@@ -18,12 +35,13 @@ class CaesarCipherClient{
 
         System.out.println("\n***********************************************************************");
         System.out.println("This program creates a client to communicate with a CaesarCipherServer.");
-        System.out.println("\nBegin by entering the number of places to shift each letter. Then,");
-        System.out.println("enter messages to encode.");
-        System.out.println("\nType 'quit' to exit.");
+        System.out.println("\nBegin by entering the number of places to shift each letter (int, 1-25 inclusive).");
+        System.out.println("Then, enter messages to encode.");
+        System.out.println("\nType 'quit' and hit enter to exit.");
         System.out.println("***********************************************************************\n");
 
         System.out.println("Connecting to host: " + server);
+
         try{
             // connect to server
             Socket socket = new Socket(server, 9876);
@@ -33,32 +51,28 @@ class CaesarCipherClient{
             PrintStream out = new PrintStream((socket.getOutputStream()));
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+            // scanner to accept user input
             Scanner scanner = new Scanner(System.in);
-            int shift;
 
-            while(true)
-            {
-                try{
-                    System.out.print("Enter shift amount: ");
-                    shift = Integer.parseInt(scanner.nextLine());
-                    if(shift > 25 || shift < 1){
-                        throw new Exception("bad range");
-                    }
-                    break;
-                }
-                catch(Exception e)
-                {
-                    System.out.println("Shift must be an integer, 1-25 inclusive. Try again.");
-                }
+            // prompt for and send shift
+            System.out.print("Enter shift amount: ");
+            String shift = scanner.nextLine();
+            out.println(shift);
+            out.flush();
+            
+            // read shift confirmation
+            String shiftResponse = in.readLine();
+            System.out.println(shiftResponse);
+            if(shiftResponse.startsWith("Error")){
+                in.close();
+                out.close();
+                socket.close();
+                scanner.close();
+                System.out.println("Connection closed.");
+                return;
             }
 
-            // send input
-            out.println(shift);
-            
-            // read data from server until we finish reading
-            String line = in.readLine();
-            System.out.println("Confirmed shift: " + line);
-
+            // forever loop; continue processing user input until 'quit'
             while(true){
                 System.out.print("\nMessage: ");
                 String input = scanner.nextLine();
@@ -66,7 +80,26 @@ class CaesarCipherClient{
                     break;
                 }
                 out.println(input);
-                String response = in.readLine();
+                out.flush();
+
+                // wait for server response, check for disconnect
+                String response;
+                try{
+                    response = in.readLine();
+                    if(response.equals(null)){
+                        throw new Exception("disconnected");
+                    }
+                }
+                catch(Exception e){
+                    System.out.println("Connection lost. Exiting.");
+                    in.close();
+                    out.close();
+                    socket.close();
+                    scanner.close();
+                    return;
+                }
+
+                // display response
                 System.out.println("Response: " + response);
             }
 
@@ -79,6 +112,7 @@ class CaesarCipherClient{
         }
         catch(Exception e)
         {
+            System.out.println("\nError encountered:");
             e.printStackTrace();
         }
     }
--- Project_7/CaesarCipherServer.java
+++ Project_7/CaesarCipherServer.java
@@ -1,3 +1,22 @@
+/*
+CaesarCipherServer.java
+CS 232 Client-Server Computing Project
+
+Created by Ben DeWeerd on 4.18.2022
+
+The CaesarCipherServer class creates a web server to interact with
+CaesarCipherClient objects. After establishing a connection and
+determining the shift amount, the server accepts input, encodes
+it using the Caesar Cipher, and returns the encoded version.
+
+The server uses the standard Socket and ServerSocket classes 
+for networking and spawns threads to handle multiple clients
+at a time.
+
+The tutorials found here were very helpful:
+https://www.infoworld.com/article/2853780/socket-programming-for-scalable-systems.html
+*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,6 +25,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.text.SimpleDateFormat;  
 import java.util.Date;  
+
 
 public class CaesarCipherServer extends Thread {
     private ServerSocket serverSocket;
@@ -35,12 +55,10 @@ public class CaesarCipherServer extends Thread {
         running = true;
         while (running) {
             try {
-                System.out.println("Listening for a connection...");
-
                 // receive the next connection
                 Socket socket = serverSocket.accept();
 
-                // pass socket to RequestHandler thread for processing
+                // create new requestHandler thread for new client
                 RequestHandler requestHandler = new RequestHandler(socket);
                 requestHandler.start();
             } catch (IOException e) {
@@ -55,18 +73,21 @@ public class CaesarCipherServer extends Thread {
             System.exit(0);
         }
         int port = Integer.parseInt(args[0]);
-        System.out.println("Start server on port: " + port);
+
+        System.out.println("\n***********************************************************************");
+        System.out.println("This program creates a server to communicate with CaesarCipher Clients.");
+        System.out.println("Starting server on port: " + port);
+        System.out.println("***********************************************************************\n");
 
         CaesarCipherServer server = new CaesarCipherServer(port);
         server.startServer();
-
-        // server.stopServer();
     }
 }
 
 class RequestHandler extends Thread {
     private Socket socket;
     private SimpleDateFormat formatter;
+    private Date date;
 
     RequestHandler(Socket socket) {
         this.socket = socket;
@@ -76,40 +97,83 @@ class RequestHandler extends Thread {
     @Override
     public void run() {
         try {
-            Date date = new Date();
+            // thread has started with a new client
+            date = new Date();
             String clientIP = socket.getRemoteSocketAddress().toString();
-            System.out.println(formatter.format(date) + ": Received a connection.");
+            System.out.println("\n" + formatter.format(date) + ": Received a connection.");
             System.out.println("\tClient IP: " + clientIP);
 
             // get input and output streams
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(socket.getOutputStream());
 
-            // write out header to the client
-            int shift;
+            int shift = 0;
             try {
+                // get shift amount
                 shift = Integer.parseInt(in.readLine());
-                System.out.println(clientIP + ": shift = " + shift);
-                out.println(shift);
+                if(shift > 25 || shift < 1){
+                    throw new Exception("Bad range");
+                }
+                out.println("Confirmed shift: " + shift);
                 out.flush();
             } catch (Exception e) {
-                e.printStackTrace();
-            }
-            String line;
-            do {
-                line = in.readLine();
-                // System.out.println("Server: line = " + line);
-                out.println("Echo: " + line);
+                out.println("Error: shift must be in integer, 1-25 inclusive. Exiting.");
                 out.flush();
-            } while (line != null && line.length() > 0);
+                in.close();
+                out.close();
+                socket.close();
+                date = new Date();
+                System.out.println("\n" + formatter.format(date) + ": Connection closed.");
+                System.out.println("\tClient IP: " + clientIP);
+                return;
+            }
+
+            date = new Date();
+            System.out.println("\n" + formatter.format(date) + ": shift = " + shift);
+            System.out.println("\tClient IP: " + clientIP);
+
+            // read user input, encode it, and send the result until client disconnects
+            String line;
+            while(true){
+                line = in.readLine();
+                if(line == null){
+                    // client disconnected
+                    break;
+                }
+                line = encode(line, shift);
+                out.println(line);
+                out.flush();
+            }
 
             // close connection
             in.close();
             out.close();
             socket.close();
-            System.out.println(clientIP + ": Connection closed");
+            date = new Date();
+            System.out.println("\n" + formatter.format(date) + ": Connection closed.");
+            System.out.println("\tClient IP: " + clientIP);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    // encode string using Caesar Cipher
+    private String encode(String line, int shift){
+        // see https://stackoverflow.com/questions/19108737/java-how-to-implement-a-shift-cipher-caesar-cipher#:~:text=Add%20shift%20to%20the%20character,Return%20the%20string.
+        StringBuilder strBuilder = new StringBuilder();
+        char c;
+        // iterate through the string, shifting each character individually
+        for(int i = 0; i < line.length(); i++){
+            c = line.charAt(i);
+            if(Character.isLetter(c)){
+                c = (char)(line.charAt(i) + shift);
+            }
+            // wrap around to beginning of alphabet if necessary
+            if((Character.isLowerCase(line.charAt(i)) && c > 'z') || (Character.isUpperCase(line.charAt(i)) && c > 'Z')){
+                c = (char)(line.charAt(i) - (26 - shift));
+            }
+            strBuilder.append(c);
+        }
+        return strBuilder.toString();
     }
 }
